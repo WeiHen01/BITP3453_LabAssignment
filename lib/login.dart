11bd34978395adef94fe9890b7ca9824d4fbf334
NG WEI HEN <rolandng0001@gmail.com>
2023-12-13 22:33:22 +0800

    BITP3453_Lab Assignment

--- lib/login.dart
+++ lib/login.dart
@@ -34,86 +34,88 @@ class _LoginScreenState extends State<LoginScreen> {
         appBar: AppBar(
           title: const Text('Login Screen'),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              Padding(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: "https://w7.pngwing.com/pngs/978/821/"
+                          "png-transparent-money-finance-wallet-payment-daily"
+                          "-expenses-saving-service-personal-finance.png"),
+                ),
+                Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: FadeInImage.memoryNetwork(
-                    placeholder: kTransparentImage,
-                    image: "https://w7.pngwing.com/pngs/978/821/"
-                        "png-transparent-money-finance-wallet-payment-daily"
-                        "-expenses-saving-service-personal-finance.png"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  controller: usernameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Username',
+                  child: TextField(
+                    controller: usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                    ),
                   ),
                 ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: true, // Hide the password
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
+          
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true, // Hide the password
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
                   ),
                 ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  controller: ipAddressController,
-                  decoration: InputDecoration(
-                    labelText: "REST API address",
-                  ),),
-              ),
-
-              ElevatedButton(
-                  onPressed: () async{
-                    String username = usernameController.text;
-                    String password = passwordController.text;
-                    if(username == 'test' && password == '12345678'){
-                      final prefs = await SharedPreferences.getInstance();
-                      String ip = ipAddressController.text;
-                      await prefs.setString("ip", ip);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context)=> DailyExpensesApp(username: username),
-                        ),
-                      );
-
-                    }
-                    else {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              title: const Text('Login Failed'),
-                              content: const Text('Invalid username or password.'),
-                              actions: [
-                                TextButton(
-                                    child: const Text('OK'),
-                                    onPressed: (){
-                                      Navigator.pop(context);
-                                    }
-                                )
-                              ],
-                            );
-                          }
-                      );
-                    }
-                  },
-                  child: Text("Login")
-              ),
-            ],
+          
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: ipAddressController,
+                    decoration: InputDecoration(
+                      labelText: "REST API address",
+                    ),),
+                ),
+          
+                ElevatedButton(
+                    onPressed: () async{
+                      String username = usernameController.text;
+                      String password = passwordController.text;
+                      if(username == 'test' && password == '12345678'){
+                        final prefs = await SharedPreferences.getInstance();
+                        String ip = ipAddressController.text;
+                        await prefs.setString("ip", ip);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context)=> DailyExpensesApp(username: username),
+                          ),
+                        );
+          
+                      }
+                      else {
+                        showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text('Login Failed'),
+                                content: const Text('Invalid username or password.'),
+                                actions: [
+                                  TextButton(
+                                      child: const Text('OK'),
+                                      onPressed: (){
+                                        Navigator.pop(context);
+                                      }
+                                  )
+                                ],
+                              );
+                            }
+                        );
+                      }
+                    },
+                    child: Text("Login")
+                ),
+              ],
+            ),
           ),
         )
     );
